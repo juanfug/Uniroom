ea23571a1546b451f2e773842a8b6ad4ff8ea4e3
juanfug <54547859+juanfug@users.noreply.github.com>
2020-09-07 23:24:08 -0500

    vistas en galeria lightbox

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
-document.getElementById("menu-icono").addEventListener("click", mostrar_menu);
+//document.getElementById("menu-icono").addEventListener("click", mostrar_menu);
 
-function mostrar_menu(){
-    document.getElementById("mov-cont").classList.toggle('move-all');
-    document.getElementById("show-menu").classList.toggle('show-lateral-menu-header');
-}
+//function mostrar_menu(){
+  //  document.getElementById("mov-cont").classList.toggle('move-all');
+  //  document.getElementById("show-menu").classList.toggle('show-lateral-menu-header');
+//}
 
 /*-------------------------------------------------*/
 /*script para layout de imagenes*/
 /*-------------------------------------------------*/
 
-const grid = new Muuri('.grid',{
-    layaout:{
-        rounding:false
-    }
+//const grid = new Muuri('.grid',{
+   // layaout:{
+    //    rounding:false
+  //  }
+//});
+
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*galeria estilos modal*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+
+//variable
+
+let modale=document.querySelector('.modale');
+let images =document.querySelectorAll('.galeriap img');
+let modalImg=document.querySelector('#mymodaleimg');
+let imgBox = document.querySelectorAll('.lists img');
+let caption = document.querySelector('.caption-text');
+let close = document.querySelector('.close');
+
+
+images.forEach((images) =>{
+    images.addEventListener('click', ()=>{
+        modale.style.display = "block";
+        modalImg.src = images.src;
+        caption.innerHTML = images.alt;
+    });
+});
+
+
+imgBox.forEach(img=>img.addEventListener("click", imgLightbox));
+function imgLightbox(event){
+    modalImg.src = event.target.src;
+}
+
+close.addEventListener("click",()=>{
+    modale.style.display = "none";
 });
